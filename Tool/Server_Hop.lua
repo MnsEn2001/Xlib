307de91f0eeb599bc1d4f7648e922fc7e85555b5
MnsEn2001 <ma0979354966.js919345@gmail.com>
2024-11-17 14:02:13 +0700

    Update Server_Hop.lua

--- Tool/Server_Hop.lua
+++ Tool/Server_Hop.lua
@@ -1,23 +1,9 @@
-local Player = game.Players.LocalPlayer    
-local Http = game:GetService("HttpService")
-local TPS = game:GetService("TeleportService")
-local Api = "https://games.roblox.com/v1/games/"
-
-local _place,_id = game.PlaceId, game.JobId
-local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
-function ListServers(cursor)
-   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-   return Http:JSONDecode(Raw)
+local function Rejoin()
+    local ts = game:GetService("TeleportService")
+    local p = game:GetService("Players").LocalPlayer
+    
+    ts:Teleport(game.PlaceId, p)
 end
 
-local Next; repeat
-   local Servers = ListServers(Next)
-   for i,v in next, Servers.data do
-       if v.playing < v.maxPlayers and v.id ~= _id then
-           local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
-           if s then break end
-       end
-   end
-   
-   Next = Servers.nextPageCursor
-until not NextNext
+-- รันฟังก์ชัน Rejoin ทันทีเมื่อ script ถูกรัน
+Rejoin()
